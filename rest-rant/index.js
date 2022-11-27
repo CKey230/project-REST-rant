daa203ad9f9506c175807094e624a0de320790a3
Ckey230 <Ckey230@gmail.com>
2022-11-27 15:36:25 -0500

    Adding remaining stub routes

--- rest-rant/index.js
+++ rest-rant/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 
 const app = express()
 
-app.use('/places', require('./controllers/places'))
+app.use('/places', require('../controllers/places'))
 
 
 app.get('/', (req,res) => {
