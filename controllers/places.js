358ae974a8af774e0aa74a16472b5e5c1be0bbe4
Ckey230 <Ckey230@gmail.com>
2022-12-04 17:44:19 -0500

    Form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -4,21 +4,13 @@ router.get('/new', (req,res) => {
   res.render('places/new')
 })
 
-router.get('/', (req,res) => {
-    let places = [{
-            name: 'H-Thai-ML',
-            city: 'Seattle',
-            state: 'WA',
-            cuisines: 'Thai, Pan-Asian',
-            pic: '/images/plates.jpg'
-          }, {
-            name: 'Coding Cat Cafe',
-            city: 'Phoenix',
-            state: 'AZ',
-            cuisines: 'Coffee, Bakery',
-            pic: '/images/table.jpg'
-          }]
-          
+
+
+      
+router.post('/', (req,res) => {
+  console.log(req.body)
+  res.send('POST/places')
+})    
     
     res.render('places/index', { places })
 })
