c1c8c993ee6a4a3fdd9aa061848d834a20f68dff
Ckey230 <Ckey230@gmail.com>
2022-12-13 23:00:17 -0500

    Seeder file created

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,46 +1,58 @@
 const React = require('react')
-const places = require('../../models/places')
 const Def = require('../default')
 
 function show(data) {
-    return(
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
+
+    return (
         <Def>
             <div className="row">
                 <div className="col-sm-6">
                     <img src={data.place.pic} alt={data.place.name} width="90%" />
-                    
+                    <h3>
+                        Located in {data.place.city}, {data.place.state}
+                    </h3>
                 </div>
                 <div className="col-sm-6">
                     <h1>{data.place.name}</h1>
                     <h2>
                         Rating
                     </h2>
-                    <p>Currently Unrated</p>
+                    {rating}
                     <br />
                     <h2>
                         Description
                     </h2>
-                    
                     <h3>
-                        Located in {data.place.city}, {data.place.state}
+                        {data.place.showEstablished()}
                     </h3>
                     <h4>
                         Serving {data.place.cuisines}
                     </h4>
+                    
+                    
                 </div>
-                
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
-                
+                <hr />
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                    Edit
+                </a>    
+
+
                 <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
                     <button type="submit" className="btn btn-danger">
                         Delete
                     </button>
-                </form>
-                
-                <h2>Comments</h2>
-                <div className="row">
-                    <p>No comments yet</p>
-                </div>
+                </form>     
+               
             </div>
             
            
