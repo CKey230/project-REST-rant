e04cb4ceee4df1db2be06fb702950e62acd0c852
Ckey230 <Ckey230@gmail.com>
2022-12-21 15:41:54 -0500

    deployment

--- views/default.jsx
+++ views/default.jsx
@@ -7,6 +7,7 @@ function Def (html) {
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"/>
                 <link rel="stylesheet" href="/css/style.css"/>
+                <link rel="shortcut icon" href="http://example.com/favicon.ico"/>
             </head>
             <body>
                 <nav>
